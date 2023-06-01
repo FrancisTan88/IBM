00d93d64f1eca575205ea3aeb0f489eaed10325b
Francis Tan <kian080787@gmail.com>
2023-06-01 17:56:04 +0800

    modify

--- data_processing/ExlToInsertScript/sql.py
+++ data_processing/ExlToInsertScript/sql.py
@@ -13,11 +13,9 @@ for i in range(len(data)):
     post_code = data['Post Code'].iloc[i]
     city = data['City'].iloc[i]
     state_code = data['State code'].iloc[i]
-    string = "INSERT INTO common.SystemDropDownList(Code, Type, Name, LanguageEnglish, LanguageLocal, Description,  ParentCode, SortNo, IsEnable, IsSystemGen, CreateUserNo, CreateTime) Values" + f"('{post_code}', 'AddressPostCode', '{post_code}', '{post_code}', '{post_code}', 'AddressLevel2',  (SELECT [Code] FROM common.SystemDropDownList where Type='AddressLevel2' and Name='{city}' and ParentCode='{state_code}'), {post_code}, True, true, '00000', GETDATE())"
+    string = "INSERT INTO common.SystemDropDownList(Code, Type, Name, LanguageEnglish, LanguageLocal, Description,  ParentCode, SortNo, IsEnable, IsSystemGen, CreateUserNo, CreateTime) Values" + \
+        f"('{post_code}', 'AddressPostCode', '{post_code}', '{post_code}', '{post_code}', 'AddressLevel2',  (SELECT [Code] FROM common.SystemDropDownList where Type='AddressLevel2' and Name='{city}' and ParentCode='{state_code}'), {post_code}, True, true, '00000', GETDATE())"
     list.append(string)
 
 df = pd.DataFrame(list, columns=['SQL query'])
 df.to_csv(export_path, header=False, index=False)
-
-
-
